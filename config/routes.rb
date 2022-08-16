d080dd3c77e30fcfbac6a986ef17d48a6748dd1d
Mike Shippy <mikeship04@gmail.com>
2022-08-16 10:08:27 -0700

    commit tree cd0addb1cbfd2246581acef218539c73eb33b8f3

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  post "/login", to: "session#create"
+  delete "/logout", to: "session#destroy"
+
+  post "/signup", to: "users#create"
+  get "/me", to: "users#show"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
